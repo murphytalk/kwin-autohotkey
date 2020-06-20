bffb2b263fded47f15173ce53abfb4100e6fec87
Mu Lu <murphytalk@gmail.com>
2020-06-20 21:22:55 +0900

    hotkey for emacs an vs code

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,18 +1,21 @@
-var prevActiveClient = null;
-const CONSOLE_CAPTION = "urxvt";
+var prevActiveClient = {};
 
-function raiseConsole(){
-    if(prevActiveClient != null && workspace.activeClient.caption === CONSOLE_CAPTION){
-        workspace.activeClient = prevActiveClient;
-        prevActiveClient = null;
+function matchCaption(wndCap, caption, fullMatch){
+    return fullMatch ? wndCap  === caption : wndCap.indexOf(caption) != -1;
+}
+
+function raiseWnd(caption, fullMatch){
+    if( (caption in prevActiveClient) && matchCaption(workspace.activeClient.caption, caption, fullMatch)){
+        workspace.activeClient = prevActiveClient[caption];
+        delete prevActiveClient[caption];
         return;
     }
     var clients = workspace.clientList();
     for (var i = 0; i<clients.length; i++){
         var client = clients[i];
-        if (client.caption === CONSOLE_CAPTION){
-            if (workspace.activeClient.caption != CONSOLE_CAPTION){
-                prevActiveClient = workspace.activeClient;
+        if (matchCaption(client.caption, caption, fullMatch)){
+            if (!matchCaption(workspace.activeClient.caption, caption, fullMatch)){
+                prevActiveClient[caption] = workspace.activeClient;
             }
             workspace.activeClient = client;
             break;
@@ -20,7 +23,14 @@ function raiseConsole(){
     }
 }
 
-// F1 to bring terminal window to front
-registerShortcut ('KWin-AutoHotKey: Terminal Window', 'KWin-AutoHotKey: Terminal Window', 'F1', function(){
-    raiseConsole();
+registerShortcut ('KWin-AutoHotKey: Terminal Window', 'KWin-AutoHotKey: Terminal Window', 'Ctrl+F12', function(){
+    raiseWnd("urxvt", true);
+});
+
+registerShortcut ('KWin-AutoHotKey: Emacs Window', 'KWin-AutoHotKey: Emacs Window', 'Ctrl+Alt+E', function(){
+    raiseWnd("@emacs", false);
+});
+
+registerShortcut ('KWin-AutoHotKey: Visual Code Window', 'KWin-AutoHotKey: Visual Code Window', 'Meta+C', function(){
+    raiseWnd("Visual Studio Code", false);
 });
