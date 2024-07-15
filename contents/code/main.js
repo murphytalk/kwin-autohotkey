6a2cde5dc380c4352dfcb6a82ce5325b9dc2e0e7
Mu Lu <murphytalk@gmail.com>
2024-07-15 18:48:00 +0900

    migrated to kde 6

--- contents/code/main.js
+++ contents/code/main.js
@@ -2,27 +2,29 @@ var prevActiveClient = {};
 
 function matchWndClass(wndClass, targetClass){
     // wndClass is an ArrayByte obj, but using it it in indexOf automatcially cast it to string
+    console.log(wndClass)
     return  targetClass.indexOf(wndClass) === 0;
 }
 
 function raiseWnd(targetClass){
-    if( (targetClass in prevActiveClient) && matchWndClass(workspace.activeClient.resourceClass, targetClass)){
-        workspace.activeClient = prevActiveClient[targetClass];
+    if( (targetClass in prevActiveClient) && matchWndClass(workspace.activeWindow.resourceClass, targetClass)){
+        workspace.activeWindow = prevActiveClient[targetClass];
         delete prevActiveClient[targetClass];
         return;
     }
-    var clients = workspace.clientList();
+    var clients = workspace.stackingOrder;
     for (var i = 0; i<clients.length; i++){
         var client = clients[i];
         if (matchWndClass(client.resourceClass, targetClass)){
-            if (!matchWndClass(workspace.activeClient.resourceClass, targetClass)){
+            if (!matchWndClass(workspace.activeWindow.resourceClass, targetClass)){
                 prevActiveClient[targetClass] = workspace.activeClient;
             }
-            workspace.activeClient = client;
+            workspace.activeWindow = client;
             break;
         }
     }
 }
+
 
 registerShortcut ('KWin-AutoHotKey: Terminal Window', 'KWin-AutoHotKey: Terminal Window', 'Ctrl+Alt+C', function(){
     raiseWnd("st-256color");
@@ -37,13 +39,23 @@ registerShortcut ('KWin-AutoHotKey: Visual Code Window', 'KWin-AutoHotKey: Visua
 });
 
 registerShortcut ('KWin-AutoHotKey: Microsoft Edge', 'KWin-AutoHotKey: Microsoft Edge', 'Ctrl+Alt+Q', function(){
-    raiseWnd("microsoft-edge-beta");
+    raiseWnd("Microsoft-edge");
 });
 
 registerShortcut ('KWin-AutoHotKey: Logseq', 'KWin-AutoHotKey: Logseq', 'Ctrl+Alt+N', function(){
-    raiseWnd("logseq");
+    raiseWnd("Logseq");
+});
+
+
+//raiseWnd("Logseq");
+
+/*
+registerShortcut ('KWin-AutoHotKey: IntelliJ', 'KWin-AutoHotKey: IntelliJ', 'Meta+I', function(){
+    raiseWnd('jetbrains-studio')
 });
 
 //egisterShortcut ('KWin-AutoHotKey: Firefox Window', 'KWin-AutoHotKey: Firefox Window', 'Ctrl+Alt+F', function(){
 //   raiseWnd("firefox");
 //);
+*/
+
